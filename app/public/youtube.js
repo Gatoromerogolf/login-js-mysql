477f10c9be52fdd4ae7fe828a2854581a0f0d62e
Gatoromerogolf <ruben.e.garcia@gmail.com>
2024-05-13 00:24:21 -0300

    guia cursos

---
+++ app/public/youtube.js
@@ -0,0 +1,24 @@
+/*login con javascript y mysql
+https://www.youtube.com/watch?v=YzVYI1TxVFc
+
+Insertar registros a tabla MySQL desde formulario HTML usando Javascript y Node
+https://www.youtube.com/watch?v=aHPeMYFh5Cg
+
+login con javascript y mysql
+Registro y Login funcional en Javascript con node y express - guía paso a paso
+https://www.youtube.com/watch?v=fjuIeiA8QkE&t=33s
+
+envio de emails de verificación desde el backend
+https://www.youtube.com/watch?v=V1DkTKtQuEY
+
+mails desde el front end
+https://www.youtube.com/watch?v=DcTQxT8f7G0
+
+registro y verificacion de usuario por email Node JS y MongoDB
+https://www.youtube.com/watch?v=2NvtJJODhgo
+
+La manera más sencilla de comunicar tu sitio con un servidor de backend
+https://www.youtube.com/watch?v=KABu6tVlgmI
+
+Registro y Login funcional en Javascript con node y express - guia paso a paso
+https://www.youtube.com/watch?v=fjuIeiA8QkE&t=2185s*/
