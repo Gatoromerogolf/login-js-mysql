13c48b3f96cebe44f49a726cc0d6aafd4508f86c
Gatoromerogolf <ruben.e.garcia@gmail.com>
2024-05-30 18:05:38 -0300

    cambios

--- app/index.js
+++ app/index.js
@@ -1,19 +1,27 @@
-import express from "express";
-//Fix para direname
+import  express  from "express";
+import cookieParser from 'cookie-parser';
+//Fix para __direname
 import path from 'path';
 import {fileURLToPath} from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+import {methods as authentication} from "./controllers/authentication.controller.js"
+import {methods as authorization} from "./middlewares/authorization.js";
 
-//server 
+//Server
 const app = express();
-app.set("port", 4000);
-app.listen(app.get('port'));
-console.log ('servidor corriendo en', app.get('port'), "...");
+app.set("port",4000);
+app.listen(app.get("port"));
+console.log("Servidor corriendo en puerto",app.get("port"));
 
-//configuracion
-app.use(express.static(__dirname + '/public'));
+//Configuración
+app.use(express.static(__dirname + "/public"));
+app.use(express.json());
+app.use(cookieParser())
 
-//rutas
-app.get("/", (req, res) =>res.sendFile(__dirname + '/pages/login.html'));
-app.get("/register", (req, res) =>res.sendFile(__dirname + '/pages/register.html'));
-app.get("/admin", (req, res) =>res.sendFile(__dirname + '/pages/admin/admin.html'));
+
+//Rutas
+app.get("/",authorization.soloPublico, (req,res)=> res.sendFile(__dirname + "/pages/login.html"));
+app.get("/register",authorization.soloPublico,(req,res)=> res.sendFile(__dirname + "/pages/register.html"));
+app.get("/admin",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname + "/pages/admin/admin.html"));
+app.post("/api/login",authentication.login);
+app.post("/api/register",authentication.register);
--- app/public/register.js
+++ app/public/register.js
@@ -1,19 +1,57 @@
-document.getElementById('register-form').addEventListener('submit', async e => {
-    e.preventDefault();
-    console.log(e);
-    console.log(e.target.children.passw.value);
-    console.log(e.target.children.text.value);
-    console.log(e.target.children.user.value);
-    const res = await fetch('http://localhost:4000/api/register', {
-        method:'POST´',
-        headers:{
-            "Content-Type" : "application/json"
-        },
-        body: JSON.stringify({
-            user: e.target.children.user.value,
-            email: e.target.children.email.value,
-            passowrd: e.target.children.password.value
-        })
-});
-//Función Flecha (Arrow Function): Se usa e => directamente para definir el parámetro de la función flecha. Este es el formato correcto para las funciones flecha en JavaScript.})
+// document
+//   .getElementById("register-form")
+//   .addEventListener("submit", async (e) => {
+//     e.preventDefault();
+//     console.log(e);
+//     console.log(e.target.children.user.value);
+//     console.log(e.target.children.email.value);
+//     console.log(e.target.children.password.value);
+
+//     try {
+//       const res = await fetch("http://localhost:4000/admin", {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({
+//           user: e.target.children.user.value,
+//           email: e.target.children.email.value,
+//           password: e.target.children.password.value,
+//         }),
+//       });
+//       if (!res.ok) {
+//         throw new Error("Network response was not ok " + res.statusText);
+//       }
+
+//       const data = await res.json();
+//       console.log(data);
+//     } catch (error) {
+//       console.error(
+//         "There has been a problem with your fetch operation:",
+//         error
+//       );
+//     }
+//   });
+
+const mensajeError = document.getElementsByClassName("error")[0];
+
+document.getElementById("register-form").addEventListener("submit",async(e)=>{
+  e.preventDefault();
+  console.log(e.target.children.user.value)
+  const res = await fetch("http://localhost:4000/api/register",{
+    method:"POST",
+    headers:{
+      "Content-Type" : "application/json"
+    },
+    body: JSON.stringify({
+      user: e.target.children.user.value,
+      email: e.target.children.email.value,
+      password: e.target.children.password.value
+    })
+  });
+  if(!res.ok) return mensajeError.classList.toggle("escondido",false);
+  const resJson = await res.json();
+  if(resJson.redirect){
+    window.location.href = resJson.redirect;
+  }
 })
